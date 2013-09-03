0740c5f1de2a6cf4b117e1831980fe9bb1ae5ae9
Michael Schätzlein <schaetzl@in.tum.de>
2013-09-03 19:01:37 +0200

    starting working on MusicReader

--- music.rb
+++ music.rb
@@ -2,4 +2,14 @@
 
 module MusicReader
   MUSICDIR = "music"
+
+  def self.all_artists()
+    artists = Dir["#{MUSICDIR}/*"]
+    artists.map{ |artist| artist.gsub!("#{MUSICDIR}/", '') }
+    artists.sort!
+  end
+
+  def self.artist_albums(artist_name)
+    albums = Dir["#{MUSICDIR}/#{artist_name}/*"]
+  end
 end
